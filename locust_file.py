2a82aa42bc15b3f97675f21a3a62dce6ad683a26
northrom <fbarquero86@gmail.com>
2016-11-24 01:09:22 -0600

    adding a basic task

--- locust_file.py
+++ locust_file.py
@@ -1,7 +1,12 @@
 from locust import HttpLocust, TaskSet
 
 class UserBehavior(TaskSet):
-    pass
+
+    def get_tasks(self):
+        # client object and the requests are build in top of requests python module
+        # the base url to access is defined by the host attibute
+        # as client is an object
+        self.client.get("/")
 
 class WebsiteUser(HttpLocust):
     pass
